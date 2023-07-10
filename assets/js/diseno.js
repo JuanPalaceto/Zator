84bb54021c896faaa69d22dec67027d3aab42b3b
Juan <jpalaceto@hotmail.com>
2023-07-10 13:55:46 -0600

    galeria

--- assets/js/diseno.js
+++ assets/js/diseno.js
@@ -8,3 +8,18 @@ window.addEventListener('scroll', function() {
         element.style.transform = 'translateY(' + (-scrollOffset * scrollStrength) + 'px)';
     });
 });
+
+const glightbox2 = GLightbox ({
+    selector: '.glightbox2',
+    loop: 'true',
+});
+
+const glightbox3 = GLightbox ({
+    selector: '.glightbox3',
+    loop: 'true',
+});
+
+const glightbox4 = GLightbox ({
+    selector: '.glightbox4',
+    loop: 'true',
+});
