12085fc487bcdc9c53d7749e7121dafebebe1533
Juan <jpalaceto@hotmail.com>
2023-07-11 13:00:46 -0600

    preview videos

--- assets/js/diseno.js
+++ assets/js/diseno.js
@@ -23,3 +23,40 @@ const glightbox4 = GLightbox ({
     selector: '.glightbox4',
     loop: 'true',
 });
+
+// Para el preview del vídeo en el hover
+
+const videos = document.querySelectorAll("video");
+
+function startPreview(video) {
+  video.muted = true;
+  video.currentTime = 1;
+  video.playbackRate = 0.5;
+  video.play();
+}
+
+function stopPreview(video) {
+  video.currentTime = 0;
+  video.playbackRate = 1;
+  video.pause();
+}
+
+function handleMouseEnter(video) {
+  function playPreview() {
+    startPreview(video);
+  }
+
+  video.addEventListener("ended", playPreview);
+  playPreview();
+}
+
+function handleMouseLeave(video) {
+  video.removeEventListener("ended", handleMouseEnter);
+  stopPreview(video);
+}
+
+videos.forEach((video) => {
+  video.addEventListener("mouseenter", () => handleMouseEnter(video));
+  video.addEventListener("mouseleave", () => handleMouseLeave(video));
+});
+
